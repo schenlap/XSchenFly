schenlap/XSchenFly

commit ada2e28aef4cc8ce7eb6166391c65c21704d9f5e

[A107], mobiflight sync buttons on connect

devices/rowsfire_a107.py
@@ -663,6 +663,7 @@ class device:
         xplane_connected = True
         xp = self.xp
         create_combined_button_list_a107()
+        mf_dev.force_sync(2)
 
 
     def disconnected(self):

mobiflight_client.py
@@ -1,7 +1,7 @@
 #!./myenv/bin/python3
 from enum import Enum, IntEnum
 
-from threading import Thread
+from threading import Thread, Timer
 from time import sleep
 
 import serial
@@ -32,11 +32,13 @@ class MF:
         INFO = 10
         GET_CONFIG = 12
         CONFIG_ACTIVATED = 17
+        TRIGGER = 23 # read buttons on startup
         SET_MODUL_BRIGHTNESS = 26
         SET_SHIFT_REGISTER_PINS = 27
         ANALOG_CHANGE = 28
         INPUT_SHIFTER_CHANGE = 29
         DIGINMUX_CHANGE = 30
+        TRIGGER_DONE = 34
     
     class TYPE(Enum):
         BUTTON = 1
@@ -217,6 +219,12 @@ class MF:
         if self.activated:
             print(f"send {msg}")
         self.ser.write(bytearray(msg, 'ascii'))
+
+
+    def send_trigger(self):
+        msg = str(MF.CMD.TRIGGER.value) + ";"
+        print(f"MF: force input sync {msg}")
+        self.ser.write(bytearray(msg, 'ascii'))
     
 
     def set_pin(self, name, nr, value):
@@ -241,7 +249,12 @@ class MF:
 
     def set_modul_brightness(self, name, nr, brightness):
         print(f"set modul brightness: {name} {nr} {brightness}")
-    
+
+
+    def force_sync(self, delay):
+        t = Timer(delay, self.send_trigger)
+        t.start()
+
 
     def close(self):
         self.init = False

commit 4cf39928d83484f60a53e995e2c36c0b0891fbe7

Fix regression introduced by commit ada2e28
This commit makes it so mf_dev.force_sync() is called even if mf_dev is
not initialized, in the case where no Rowsfire A107 device is connected.
This makes the script not run properly.

Signed-off-by: Jeremie Corbier <jeremie.corbier@gmail.com>

devices/rowsfire_a107.py
@@ -649,6 +649,10 @@ class device:
     def connected(self):
         global xplane_connected
         global xp
+
+        if not mf_dev:
+            return
+
         print(f"[A107] X-Plane connected")
         xplane_get_dataref_ids(self.xp)
         print(f"[A107] subsrcibe datarefs... ", end="")